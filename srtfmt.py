f900eb199efa45a46d1f18f4e18db1ae988ea7ab
Avery Khoo <averykhoo@gmail.com>
2018-12-10 19:00:03 +0800

    add (fuzzy) months

--- srtfmt.py
+++ srtfmt.py
@@ -20,21 +20,23 @@ def format_bytes(num):
 def format_seconds(num):
     """
     string formatting
+    note that the days in a month is kinda fuzzy
     :type num: int | float
-    :rtype: str
     """
     num = abs(num)
     if num == 0:
-        return '0 seconds'
+        return u'0 seconds'
     elif num == 1:
-        return '1 second'
+        return u'1 second'
+
     if num < 1:
         # display 2 significant figures worth of decimals
-        return ('%%0.%df seconds' % (1 - int(math.floor(math.log10(abs(num)))))) % num
+        return (u'%%0.%df seconds' % (1 - int(math.floor(math.log10(abs(num)))))) % num
+
     unit = 0
-    denominators = [60.0, 60.0, 24.0, 7.0]
-    while num >= denominators[unit] and unit < 4:
+    denominators = [60.0, 60.0, 24.0, 7.0, 365.25 / 84.0, 12.0]
+    while unit < 6 and num > denominators[unit] * 0.9:
         num /= denominators[unit]
         unit += 1
-    unit = ['seconds', 'minutes', 'hours', 'days', 'weeks'][unit]
-    return ('%.2f %s' if num % 1 else '%d %s') % (num, unit[:-1] if num == 1 else unit)
+    unit = [u'seconds', u'minutes', u'hours', u'days', u'weeks', u'months', u'years'][unit]
+    return (u'%.2f %s' if num % 1 else u'%d %s') % (num, unit[:-1] if num == 1 else unit)
